46d39807879a09bd5dced7478f6f07516fd5f09e
Frank Farfan <frankfarfan96@gmail.com>
2022-09-09 16:24:52 +0200

    Added 2 new components chart and featured and installed the library react-circular-progressbar

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,17 @@
+import "./featured.scss";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+const Featured = () => {
+  return (
+    <div className="featured">
+      <div className="top">
+        <h1 className="title">Total Revenue</h1>
+        <MoreVertIcon fontSize="small" />
+      </div>
+      <div className="botton">
+        <div className="featuredChart"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
