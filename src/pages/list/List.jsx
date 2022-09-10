c773700bd13b739cbcf5b0002407e8b56bf2ef74
Frank Farfan <frankfarfan96@gmail.com>
2022-09-10 12:13:27 +0200

    Added new component Datatable and installed library @mui/x-data-grid

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,7 +1,17 @@
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
 import "./list.scss";
 
 const List = () => {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        datatable
+      </div>
+    </div>
+  );
 };
 
 export default List;
