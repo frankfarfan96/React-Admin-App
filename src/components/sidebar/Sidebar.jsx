0fa8ab800769b029568d6cdea9623966bc5a160c
Frank Farfan <frankfarfan96@gmail.com>
2022-09-12 12:20:50 +0200

    Implemented react-route-dom Links in Home to Users, Products, and in Datatable Links to new user and view single user

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -12,12 +12,15 @@ import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
 import SettingsApplicationsRoundedIcon from "@mui/icons-material/SettingsApplicationsRounded";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import ExitToAppOutlinedIcon from "@mui/icons-material/ExitToAppOutlined";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="top">
-        <span className="logo">FFadmin</span>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">FFadmin</span>
+        </Link>
       </div>
       <hr />
       <div className="center">
@@ -28,14 +31,18 @@ const Sidebar = () => {
             <span>Dashboard</span>
           </li>
           <p className="title">LISTS</p>
-          <li>
-            <PersonOutlineOutlinedIcon className="icon" />
-            <span>Users</span>
-          </li>
-          <li>
-            <StoreRoundedIcon className="icon" />
-            <span>Products</span>
-          </li>
+          <Link to="/users" style={{ textDecoration: "none" }}>
+            <li>
+              <PersonOutlineOutlinedIcon className="icon" />
+              <span>Users</span>
+            </li>
+          </Link>
+          <Link to="/products" style={{ textDecoration: "none" }}>
+            <li>
+              <StoreRoundedIcon className="icon" />
+              <span>Products</span>
+            </li>
+          </Link>
           <li>
             <PaymentRoundedIcon className="icon" />
             <span>Orders</span>
